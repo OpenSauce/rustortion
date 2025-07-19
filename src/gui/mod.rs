897260c2c26a5622cbd33280ccad21dd98f608c8
OpenSauce <lawleycobhc@gmail.com>
2025-07-19 17:01:27 +0100

    Rename

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -9,12 +9,12 @@ pub use messages::Message;
 
 use crate::io::manager::ProcessorManager;
 
-pub const ICONS_FONT: Font = Font::MONOSPACE;
+pub const DEFAULT_FONT: Font = Font::MONOSPACE;
 
 pub fn start(processor_manager: ProcessorManager) -> iced::Result {
     iced::application("Rustortion", AmplifierApp::update, AmplifierApp::view)
         .window_size((800.0, 600.0))
         .theme(AmplifierApp::theme)
-        .default_font(ICONS_FONT)
+        .default_font(DEFAULT_FONT)
         .run_with(move || (AmplifierApp::new(processor_manager), iced::Task::none()))
 }
