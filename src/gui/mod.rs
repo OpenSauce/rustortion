4d3f5cf1c761780e0feda8cc534cb57c1df26801
OpenSauce <lawleycobhc@gmail.com>
2025-10-18 10:47:16 +0100

    Clean up window sizing slightly

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -16,7 +16,10 @@ pub const DEFAULT_FONT: Font = Font::MONOSPACE;
 pub fn start(processor_manager: ProcessorManager, settings: Settings) -> iced::Result {
     iced::application("Rustortion", AmplifierApp::update, AmplifierApp::view)
         .subscription(AmplifierApp::subscription)
-        .window_size((800.0, 600.0))
+        .window(iced::window::Settings {
+            min_size: Some(iced::Size::new(800.0, 600.0)),
+            ..iced::window::Settings::default()
+        })
         .theme(AmplifierApp::theme)
         .default_font(DEFAULT_FONT)
         .run_with(move || {
